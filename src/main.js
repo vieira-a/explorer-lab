a6463cd0889954e5e037c44326c1d0d6ccde374d
Anderson Vieira <asvieira.br@gmail.com>
2022-10-19 14:50:05 -0300

    refactor: dinamic handle cc brand

--- src/main.js
+++ src/main.js
@@ -5,23 +5,20 @@ const ccNumber = document.querySelector(".cc-number");
 const ccLogo = document.querySelector(".cc-logo :nth-child(2)");
 const ccInput = document.getElementById("card-number");
 
-const ccBrands = [
-  {
-    name: "default",
-    imgPath: "../public/cc-default.svg",
-    alt: "Cartão de Crédito não informado"
+const ccBrands = {
+  "default": {
+    "imgPath": "../public/cc-default.svg",
+    "alt": "Cartão de Crédito não informado"
   },
-  {
-  name: "visa",
-  imgPath: "../public/cc-visa.svg",
-  alt: "Cartão de Crédito Visa"
+  "visa": {
+    "imgPath": "../public/cc-visa.svg",
+    "alt": "Cartão de Crédito Visa"
   },
-  {
-  name: "mastercard",
-  imgPath: "../public/cc-mastercard.svg",
-  alt: "Cartão de Crédito Mastercard"
+  "mastercard": {
+    "imgPath": "../public/cc-mastercard.svg",
+    "alt": "Cartão de Crédito Mastercard"
   },
-]
+}
 
 const handleCCNumber = () => {
   if(ccNumber.textContent === ''){
@@ -55,15 +52,7 @@ const handleCCInputNumber = () => {
 }
 
 const handleCCBrand = (brand) => {
-  
-  if(brand === "visa") {
-    ccLogo.innerHTML = `<img src=${ccBrands[1].imgPath}>`
-  } else if(brand === "mastercard"){
-    ccLogo.innerHTML = `<img src=${ccBrands[2].imgPath}>`
-  } else {
-    ccLogo.innerHTML = `<img src=${ccBrands[0].imgPath}>`
-  }
-
+  ccLogo.innerHTML = `<img src=${ccBrands[brand].imgPath}>`
 }
 
 handleCCInputNumber()
